02a1a59f4bb430872567682a70854d3f0a935418
Vladimir Kozhevnikov <vladimir.kozhevnikov@touchin.ru>
2020-06-19 13:10:57 +0300

    add new map extensions (#6)
    Co-authored-by: Kozhevnikov <vladimir.kozhevnikov@revolut.com>

--- core/src/main/java/com/revolut/rxdata/core/extensions/DataExtensions.kt
+++ core/src/main/java/com/revolut/rxdata/core/extensions/DataExtensions.kt
@@ -2,6 +2,22 @@ package com.revolut.rxdata.core.extensions
 
 import com.revolut.rxdata.core.Data
 
+/*
+ * Copyright (C) 2020 Revolut
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 fun <T> Data<T>.isEmpty(): Boolean = content == null
 
@@ -23,4 +39,24 @@ inline fun <T, R> Data<T>.mapData(block: (T) -> R?): Data<R> = try {
         error = t,
         loading = loading
     )
+}
+
+inline fun <T> Data<T>.mapDataErrorToContent(block: (Throwable) -> T?): Data<T> {
+    return if (error != null) {
+        try {
+            Data(
+                content = block(error),
+                error = null,
+                loading = loading
+            )
+        } catch (t: Throwable) {
+            Data(
+                content = null,
+                error = t,
+                loading = loading
+            )
+        }
+    } else {
+        this
+    }
 }
