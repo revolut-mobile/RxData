61386fb142a2f1ca2378c3882057d52cdce0e2bd
Pavlo Ratushnyi <pavelratushniy@gmail.com>
2023-01-23 19:13:47 +0100

    Add more tests

--- flow-extensions/src/test/kotlin/com/revolut/flowdata/extensions/LoadingExtensionsKtTest.kt
+++ flow-extensions/src/test/kotlin/com/revolut/flowdata/extensions/LoadingExtensionsKtTest.kt
@@ -65,18 +65,79 @@ class LoadingExtensionsKtTest {
     //region takeUntilLoaded
 
     @Test
-    fun `Loaded item is returned and terminates the stream`() = runTest {
-        flowOf(
-            Data("A", null, loading = true),
-            Data("B", null, loading = false),
-            Data("C", null, loading = true),
-        ).takeUntilLoaded().test {
-            assertEquals(Data("A", null, loading = true), awaitItem())
-            assertEquals(Data("B", null, loading = false), awaitItem())
-            awaitComplete()
-            expectNoEvents()
+    fun `Loading item and loaded item are returned, and loaded item terminates the stream`() =
+        runTest {
+            flowOf(
+                Data("A", null, loading = true),
+                Data("B", null, loading = false),
+                Data("C", null, loading = true),
+            ).takeUntilLoaded().test {
+                assertEquals(Data("A", null, loading = true), awaitItem())
+                assertEquals(Data("B", null, loading = false), awaitItem())
+                awaitComplete()
+                expectNoEvents()
+            }
         }
-    }
+
+    @Test
+    fun `Loaded item, which is the first and the only item in the stream, is returned and terminates the stream`() =
+        runTest {
+            flowOf(
+                Data("A", null, loading = false),
+            ).takeUntilLoaded().test {
+                assertEquals(Data("A", null, loading = false), awaitItem())
+                awaitComplete()
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `Loading item with error and loaded item are returned, and loaded item terminates the stream`() =
+        runTest {
+            val error = IllegalStateException()
+
+            flowOf(
+                Data("A", error, loading = true),
+                Data("B", null, loading = false),
+                Data("C", null, loading = true),
+            ).takeUntilLoaded().test {
+                assertEquals(Data("A", error, loading = true), awaitItem())
+                assertEquals(Data("B", null, loading = false), awaitItem())
+                awaitComplete()
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `Loading item and loaded item with error are returned, and loaded item with error terminates the stream`() =
+        runTest {
+            val error = IllegalStateException()
+
+            flowOf(
+                Data("A", null, loading = true),
+                Data("B", error, loading = false),
+                Data("C", null, loading = true),
+            ).takeUntilLoaded().test {
+                assertEquals(Data("A", null, loading = true), awaitItem())
+                assertEquals(Data("B", error, loading = false), awaitItem())
+                awaitComplete()
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `Loaded item with error, which is the first and the only item in the stream, is returned and terminates the stream`() =
+        runTest {
+            val error = IllegalStateException()
+
+            flowOf(
+                Data("A", error, loading = false),
+            ).takeUntilLoaded().test {
+                assertEquals(Data("A", error, loading = false), awaitItem())
+                awaitComplete()
+                expectNoEvents()
+            }
+        }
 
     //endregion
 }
