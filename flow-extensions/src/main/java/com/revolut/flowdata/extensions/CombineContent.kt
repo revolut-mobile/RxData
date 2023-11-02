4b52f0d9355761a4896d6b752bd9051c6737787a
Yurii Kushyk <yurii.kushyk@revolut.com>
2023-11-02 11:42:07 +0200

    Added combine extensions for flow

--- flow-extensions/src/main/java/com/revolut/flowdata/extensions/CombineContent.kt
+++ flow-extensions/src/main/java/com/revolut/flowdata/extensions/CombineContent.kt
@@ -51,6 +51,98 @@ fun <T : Any> combineContent(
     )
 }
 
+inline fun <A : Any, B : Any, C : Any, D : Any, R : Any> combineContent(
+    a: Flow<Data<A>>,
+    b: Flow<Data<B>>,
+    c: Flow<Data<C>>,
+    d: Flow<Data<D>>,
+    crossinline block: suspend (A, B, C, D) -> R,
+): Flow<Data<R>> = combineContent(combineContent(a, b, c), d)
+    .mapContentSuspended { (abc, d) -> block(abc.first, abc.second, abc.third, d) }
+
+inline fun <A : Any, B : Any, C : Any, D : Any, E : Any, R : Any> combineContent(
+    a: Flow<Data<A>>,
+    b: Flow<Data<B>>,
+    c: Flow<Data<C>>,
+    d: Flow<Data<D>>,
+    e: Flow<Data<E>>,
+    crossinline block: suspend (A, B, C, D, E) -> R,
+): Flow<Data<R>> = combineContent(combineContent(a, b, c), combineContent(d, e))
+    .mapContentSuspended { (abc, de) -> block(abc.first, abc.second, abc.third, de.first, de.second) }
+
+inline fun <A : Any, B : Any, C : Any, D : Any, E : Any, F : Any, R : Any> combineContent(
+    a: Flow<Data<A>>,
+    b: Flow<Data<B>>,
+    c: Flow<Data<C>>,
+    d: Flow<Data<D>>,
+    e: Flow<Data<E>>,
+    f: Flow<Data<F>>,
+    crossinline block: suspend (A, B, C, D, E, F) -> R,
+): Flow<Data<R>> = combineContent(combineContent(a, b, c), combineContent(d, e, f))
+    .mapContentSuspended { (abc, def) -> block(abc.first, abc.second, abc.third, def.first, def.second, def.third) }
+
+inline fun <A : Any, B : Any, C : Any, D : Any, E : Any, F : Any, G : Any, R : Any> combineContent(
+    a: Flow<Data<A>>,
+    b: Flow<Data<B>>,
+    c: Flow<Data<C>>,
+    d: Flow<Data<D>>,
+    e: Flow<Data<E>>,
+    f: Flow<Data<F>>,
+    g: Flow<Data<G>>,
+    crossinline block: suspend (A, B, C, D, E, F, G) -> R,
+): Flow<Data<R>> = combineContent(combineContent(a, b, c), combineContent(d, e, f), g)
+    .mapContentSuspended { (abc, def, g) -> block(abc.first, abc.second, abc.third, def.first, def.second, def.third, g) }
+
+inline fun <A : Any, B : Any, C : Any, D : Any, E : Any, F : Any, G : Any, H : Any, R : Any> combineContent(
+    a: Flow<Data<A>>,
+    b: Flow<Data<B>>,
+    c: Flow<Data<C>>,
+    d: Flow<Data<D>>,
+    e: Flow<Data<E>>,
+    f: Flow<Data<F>>,
+    g: Flow<Data<G>>,
+    h: Flow<Data<H>>,
+    crossinline block: suspend (A, B, C, D, E, F, G, H) -> R,
+): Flow<Data<R>> = combineContent(combineContent(a, b, c), combineContent(d, e, f), combineContent(g, h))
+    .mapContentSuspended { (abc, def, gh) ->
+        block(
+            abc.first,
+            abc.second,
+            abc.third,
+            def.first,
+            def.second,
+            def.third,
+            gh.first,
+            gh.second
+        )
+    }
+
+inline fun <A : Any, B : Any, C : Any, D : Any, E : Any, F : Any, G : Any, H : Any, I : Any, R : Any> combineContent(
+    a: Flow<Data<A>>,
+    b: Flow<Data<B>>,
+    c: Flow<Data<C>>,
+    d: Flow<Data<D>>,
+    e: Flow<Data<E>>,
+    f: Flow<Data<F>>,
+    g: Flow<Data<G>>,
+    h: Flow<Data<H>>,
+    i: Flow<Data<I>>,
+    crossinline block: suspend (A, B, C, D, E, F, G, H, I) -> R,
+): Flow<Data<R>> = combineContent(combineContent(a, b, c), combineContent(d, e, f), combineContent(g, h, i))
+    .mapContentSuspended { (abc, def, ghi) ->
+        block(
+            abc.first,
+            abc.second,
+            abc.third,
+            def.first,
+            def.second,
+            def.third,
+            ghi.first,
+            ghi.second,
+            ghi.third
+        )
+    }
+
 fun <T : Any> combineContent(
     vararg flows: Flow<Data<T>>
 ): Flow<Data<List<T>>> = combineContent(flows.toList())
